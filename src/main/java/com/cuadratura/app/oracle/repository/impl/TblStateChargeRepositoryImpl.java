991b343916afd214c61350cb85c0da91800c7318
carangop <109491792+carangop@users.noreply.github.com>
2022-10-01 09:55:09 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/oracle/repository/impl/TblStateChargeRepositoryImpl.java
+++ src/main/java/com/cuadratura/app/oracle/repository/impl/TblStateChargeRepositoryImpl.java
@@ -20,17 +20,17 @@ public class TblStateChargeRepositoryImpl implements TblStateChargeRepositoryCus
 	@Qualifier("jdbctemplateOne")
 	private JdbcTemplate jdbcTemplate;
 
-	@Override	
+	@Override
 	public List<TblStateChargeDto> getFindAllTblStateCharge() {
 
-		String sql = "	SELECT  /*+ ALL_ROWS */  DISTINCT id_state_charge,		" + "	                load_table,		"
-				+ "	                state_charge,		" + "	                state_send,		"
-				+ "	                init_count_source,		" + "	                init_sum_source,		"
-				+ "	                end_count_target,		" + "	                end_count_source,		"
-				+ "	                end_sum_target,		" + "	                error_des,		"
-				+ "	                load_date,		" + "	                init_load,		"
-				+ "	                end_load,		" + "	                y.org_lvl_child		"
-				+ "	  FROM cuadraturawyp.tbl_state_charge x,		"
+		String sql = "	SELECT  /*+ ALL_ROWS */  DISTINCT id_state_charge,		"
+				+ "	                load_table,		" + "	                state_charge,		"
+				+ "	                state_send,		" + "	                init_count_source,		"
+				+ "	                init_sum_source,		" + "	                end_count_target,		"
+				+ "	                end_count_source,		" + "	                end_sum_target,		"
+				+ "	                error_des,		" + "	                load_date,		"
+				+ "	                init_load,		" + "	                end_load,		"
+				+ "	                y.org_lvl_child		" + "	  FROM cuadraturawyp.tbl_state_charge x,		"
 				+ "	       cuadraturawyp.tbl_hst_ft_fapinvbalee y		"
 				+ "	 WHERE     x.id_state_charge = y.audit_number		" + "	       AND x.load_table = 'HS'		"
 				+ "	       AND x.state_send = 1		";
@@ -41,9 +41,7 @@ public class TblStateChargeRepositoryImpl implements TblStateChargeRepositoryCus
 				rs.getString("load_table"), rs.getString("state_charge"), rs.getString("state_send"),
 				rs.getInt("init_count_source"), rs.getBigDecimal("init_sum_source"), rs.getInt("end_count_target"),
 				rs.getInt("end_count_source"), rs.getBigDecimal("end_sum_target"), rs.getString("error_des"),
-				rs.getDate("load_date"), rs.getDate("init_load"), rs.getDate("end_load"), rs.getLong("org_lvl_child"))
-
-		);
+				rs.getDate("load_date"), rs.getDate("init_load"), rs.getDate("end_load"), rs.getLong("org_lvl_child")));
 	}
 
 	public void updateTblStateCharge(Integer idStateCharge) {
